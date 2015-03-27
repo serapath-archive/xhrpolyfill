29f38645f29e16fabf0eecb060dcdf3ed9cbea05
serapath (Alexander Praetorius) <dev@serapath.de>
2015-03-27 23:17:29 +0100

    ADD cross browser XMLHttpRequest object

---
+++ index.js
@@ -0,0 +1,15 @@
+var factories = [
+  function () {return new XMLHttpRequest();}, // IE10+, Firefox, Chrome, Opera, Safari
+  function () {return new ActiveXObject("Msxml3.XMLHTTP");},     // IE9
+  function () {return new ActiveXObject("Msxml2.XMLHTTP.6.0");}, // IE8
+  function () {return new ActiveXObject("Msxml2.XMLHTTP.3.0");}, // IE7
+  function () {return new ActiveXObject("Msxml2.XMLHTTP");},     // IE6
+  function () {return new ActiveXObject("Microsoft.XMLHTTP");},  // IE5
+  function () {return null;}
+];
+module.exports = function getXHR() {
+  for (var i=0, xhr, len=factories.length; i<len; i++) {
+    try       { xhr = factories[i](); return xhr; }
+    catch (e) { continue; }
+  }
+};
